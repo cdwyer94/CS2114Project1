e18a8a563ab37f4f9f2120ef11bfa48ac18cb51a
cdwyer94 <cdwyer94@vt.edu>
2015-09-08 12:31:31 -0400

    Initial commit

---
+++ src/project1/DisplayCollection.java
@@ -0,0 +1,32 @@
+
+package project1;
+import bag.Bag; 
+import bag.BagInterface;
+import CS2114.TextShape; 
+import java.util.Random;
+import CS2114.Window; 
+import CS2114.Button; 
+import CS2114.WindowSide;
+
+/**
+ * @author <Christian Dwyer> <cdwyer94>
+ * @version <8/28/2015>
+ */
+public class DisplayCollection {
+
+	static String[] STRINGS;
+	static BagInterface<String> itemBag;
+	
+	public DisplayCollection() {
+		//insert code
+	}
+	
+	public void main(String[] string) {
+		//insert code
+	}
+	
+	public BagInterface<String> getItems() {
+		//insert code
+		return null;
+	}
+}
---
+++ src/project1/ShapeWindow.java
@@ -0,0 +1,33 @@
+package project1;
+import bag.Bag; 
+import bag.BagInterface;
+import CS2114.TextShape; 
+import java.util.Random;
+import CS2114.Window; 
+import CS2114.Button; 
+import CS2114.WindowSide;
+
+
+/**
+ * @author <Christian Dwyer> <cdwyer94>
+ * @version <8/28/2015>
+ */
+public class ShapeWindow {
+
+	private Window myWindow;
+	private TextShape myText;
+	private BagInterface<String> itemBag;
+	
+	public ShapeWindow(BagInterface<String> itemBag) {
+		//this.itemBag = itemBag;
+		//insert code
+	}
+	
+	public void clickedQuit(Button quit) {
+		//insert code
+	}
+	
+	public void clickedChoose(Button choose) {
+		//insert code
+	}
+}
